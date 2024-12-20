50eb76bf6aaf2b55dc46db391f020db8b317b88b
Han Kyobeom <ryqja18@gmail.com>
2024-12-20 20:09:06 +0900

    feat: EnableJapAuditing add

--- src/main/java/spartaspringnewspeed/spartafacespeed/SpartaFacespeedApplication.java
+++ src/main/java/spartaspringnewspeed/spartafacespeed/SpartaFacespeedApplication.java
@@ -2,8 +2,10 @@ package spartaspringnewspeed.spartafacespeed;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SpartaFacespeedApplication {
 
     public static void main(String[] args) {
