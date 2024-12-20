b5490efa364a826cee1cd1b12957feadde66b405
Han Kyobeom <ryqja18@gmail.com>
2024-12-20 14:55:08 +0900

    :tada: init: initial setting

---
+++ src/main/java/spartaspringnewspeed/spartafacespeed/SpartaFacespeedApplication.java
@@ -0,0 +1,13 @@
+package spartaspringnewspeed.spartafacespeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartaFacespeedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpartaFacespeedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/spartaspringnewspeed/spartafacespeed/SpartaFacespeedApplicationTests.java
@@ -0,0 +1,13 @@
+package spartaspringnewspeed.spartafacespeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpartaFacespeedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
