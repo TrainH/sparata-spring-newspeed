360a228374de169c986fee07f56c6657acdc402d
JaeH-H <hwangjh7887@gmail.com>
2024-12-23 10:19:52 +0900

    :tada: add : User Repository

--- src/main/java/spartaspringnewspeed/spartafacespeed/user/repository/UserRepository.java
+++ src/main/java/spartaspringnewspeed/spartafacespeed/user/repository/UserRepository.java
@@ -1,4 +1,12 @@
 package spartaspringnewspeed.spartafacespeed.user.repository;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import spartaspringnewspeed.spartafacespeed.common.entity.User;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUserId(Long userId);
+
 }
