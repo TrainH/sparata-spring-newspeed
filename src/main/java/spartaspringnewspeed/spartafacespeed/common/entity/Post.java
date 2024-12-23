af13a991e0fe76a50f523678b2e4462ceb5481cb
t2023-m0014 <qa9377@gmail.com>
2024-12-23 10:55:46 +0900

    add PostRepository

--- src/main/java/spartaspringnewspeed/spartafacespeed/common/entity/Post.java
+++ src/main/java/spartaspringnewspeed/spartafacespeed/common/entity/Post.java
@@ -1,14 +1,11 @@
 package spartaspringnewspeed.spartafacespeed.common.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
-public class Post {
+public class Post extends BaseEntity {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
 
     @Column
