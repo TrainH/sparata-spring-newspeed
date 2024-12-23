f149ddac627e25178d554258a3035ea48899a9f3
JaeH-H <hwangjh7887@gmail.com>
2024-12-23 09:23:25 +0900

    :sparkles update : User Entity

--- src/main/java/spartaspringnewspeed/spartafacespeed/common/entity/User.java
+++ src/main/java/spartaspringnewspeed/spartafacespeed/common/entity/User.java
@@ -1,11 +1,36 @@
 package spartaspringnewspeed.spartafacespeed.common.entity;
 
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
-@Table(name = "")
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "user")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(nullable = false)
+    private String userName;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Profile profile;
+
+    public User(String userName, String email, String password) {
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+    }
 }
