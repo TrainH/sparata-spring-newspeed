afde384f2ca017eac2d4da4757774fc975c5209d
t2023-m0014 <qa9377@gmail.com>
2024-12-23 10:45:03 +0900

    add PostRepository

--- src/main/java/spartaspringnewspeed/spartafacespeed/common/entity/Post.java
+++ src/main/java/spartaspringnewspeed/spartafacespeed/common/entity/Post.java
@@ -1,4 +1,19 @@
 package spartaspringnewspeed.spartafacespeed.common.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
 public class Post {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column
+    private String title;
+
+    @Column
+    private String content;
 }
--- src/main/java/spartaspringnewspeed/spartafacespeed/post/controller/PostController.java
+++ src/main/java/spartaspringnewspeed/spartafacespeed/post/controller/PostController.java
@@ -1,4 +1,19 @@
 package spartaspringnewspeed.spartafacespeed.post.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import spartaspringnewspeed.spartafacespeed.post.model.dto.PostDto;
+
+@Controller
 public class PostController {
+    @GetMapping("/posts/new")
+    public String newPostForm() {
+        return "posts/new";
+    }
+    @PostMapping("/posts/create")
+    public String createPost(PostDto postDto) {
+
+        return "posts/create";
+    }
 }
