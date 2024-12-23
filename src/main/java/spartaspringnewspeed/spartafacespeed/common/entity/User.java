eac0b1d0be6783e6e4fb0d62183f4fc17874c25b
JaeH-H <hwangjh7887@gmail.com>
2024-12-23 09:25:54 +0900

    :sparkles: update : User Entity

--- src/main/java/spartaspringnewspeed/spartafacespeed/common/entity/User.java
+++ src/main/java/spartaspringnewspeed/spartafacespeed/common/entity/User.java
@@ -12,6 +12,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Table(name = "user")
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;
