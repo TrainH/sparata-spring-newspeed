b2c47eb2a9e24bd7b4169b1430ea8695ee57896c
HEEHYUN0221 <psr4580@gmail.com>
2024-12-23 16:19:36 +0900

    feat : comment CRUD

--- src/main/java/spartaspringnewspeed/spartafacespeed/comment/repository/CommentRepository.java
+++ src/main/java/spartaspringnewspeed/spartafacespeed/comment/repository/CommentRepository.java
@@ -1,11 +1,17 @@
 package spartaspringnewspeed.spartafacespeed.comment.repository;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
+import spartaspringnewspeed.spartafacespeed.comment.model.dto.CommentDto;
+import spartaspringnewspeed.spartafacespeed.comment.model.dto.CommentPagingDto;
 import spartaspringnewspeed.spartafacespeed.comment.service.CommentService;
 import spartaspringnewspeed.spartafacespeed.common.entity.Comment;
 
@@ -13,4 +19,13 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
     default Comment findByIdOrElseThrow(Long id) {
         return findById(id).orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND,"Does not exist id = " + id));
     }
+
+    @Query("SELECT new spartaspringnewspeed.spartafacespeed.comment.model.dto.CommentPagingDto(" +
+            "c.id, c.user.userId, c.post.id, c.content, c.createdAt, c.updatedAt, COUNT(c.id)) " +
+            "FROM Comment c WHERE c.post.id = :postId " +
+            "GROUP BY c.id, c.user.userId, c.post.id, c.content, c.createdAt, c.updatedAt " +
+            "ORDER BY c.createdAt DESC")
+    Page<CommentPagingDto> findAllByPostIdWithCount(@Param("postId") Long postId, Pageable pageable);
+
 }
+
