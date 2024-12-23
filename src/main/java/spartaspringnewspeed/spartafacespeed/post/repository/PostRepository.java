9f3943af815a0830310287a5e239101d269343c8
Shin-Gaeul <kjkjkj8585@naver.com>
2024-12-23 14:10:04 +0900

    Update PostRepository.java

--- src/main/java/spartaspringnewspeed/spartafacespeed/post/repository/PostRepository.java
+++ src/main/java/spartaspringnewspeed/spartafacespeed/post/repository/PostRepository.java
@@ -12,7 +12,7 @@ public interface PostRepository extends JpaRepository<Post, Long> {
 
   Optional<Post> findById(Long id);
 
-    default Post findPostById(Long id) {
+    default Post findPostByIdOrThrow(Long id) {
         return findById(id)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Does not exist postId = " + id));
     }
