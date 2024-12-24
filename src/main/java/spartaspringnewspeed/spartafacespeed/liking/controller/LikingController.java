546ec011fdb377baf01d8867b9c57d5dc5d1e002
HEEHYUN0221 <psr4580@gmail.com>
2024-12-24 09:48:29 +0900

    :tada: add : LikingService

--- src/main/java/spartaspringnewspeed/spartafacespeed/liking/controller/LikingController.java
+++ src/main/java/spartaspringnewspeed/spartafacespeed/liking/controller/LikingController.java
@@ -1,4 +1,10 @@
 package spartaspringnewspeed.spartafacespeed.liking.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class LikingController {
 }
---
+++ src/main/java/spartaspringnewspeed/spartafacespeed/liking/service/LikingService.java
@@ -0,0 +1,4 @@
+package spartaspringnewspeed.spartafacespeed.liking.service;
+
+public class LikingService {
+}
