404a708a9098173338efd8d1d3f778b8ed88c55b
JaeH-H <hwangjh7887@gmail.com>
2024-12-24 15:45:26 +0900

    :adhesive_bandage: fix : Profile 기능 추가

---
+++ src/main/java/spartaspringnewspeed/spartafacespeed/user/model/request/ProfileRequest.java
@@ -0,0 +1,14 @@
+package spartaspringnewspeed.spartafacespeed.user.model.request;
+
+import lombok.Getter;
+
+@Getter
+public class ProfileRequest {
+    private String profileName;
+    private String profileEmail;
+
+    public ProfileRequest(String profileName, String profileEmail) {
+        this.profileName = profileName;
+        this.profileEmail = profileEmail;
+    }
+}
---
+++ src/main/java/spartaspringnewspeed/spartafacespeed/user/model/response/ProfileResponse.java
@@ -0,0 +1,27 @@
+package spartaspringnewspeed.spartafacespeed.user.model.response;
+
+
+import lombok.Getter;
+import spartaspringnewspeed.spartafacespeed.common.entity.User;
+
+@Getter
+public class ProfileResponse {
+
+    private Long userId;
+
+    private String profileName;
+
+    private String profileEmail;
+
+    public ProfileResponse(Long userId, String profileName, String profileEmail) {
+        this.userId = userId;
+        this.profileName = profileName;
+        this.profileEmail = profileEmail;
+    }
+
+    public static ProfileResponse toDto(User user) {
+
+        return new ProfileResponse(user.getUserId(), user.getUserName(), user.getEmail());
+    }
+
+}
