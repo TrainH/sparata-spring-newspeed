e812e7397f2095c31cad75c35ae7e746e0858f87
JaeH-H <hwangjh7887@gmail.com>
2024-12-24 09:35:00 +0900

    :tada: add : User ProfileResponse

---
+++ src/main/java/spartaspringnewspeed/spartafacespeed/user/model/response/ProfileResponse.java
@@ -0,0 +1,26 @@
+package spartaspringnewspeed.spartafacespeed.user.model.response;
+
+
+import lombok.Getter;
+import spartaspringnewspeed.spartafacespeed.common.entity.User;
+
+@Getter
+public class ProfileResponse {
+
+    private Long userId;
+
+    private String profileName;
+
+    private String profileEmail;
+
+    public ProfileResponse(Long userId, String profileName, String profileEmail) {
+        this.userId = userId;
+        this.profileName = profileName;
+        this.profileEmail = profileEmail;
+    }
+
+    public static ProfileResponse toDto(User user) {
+
+        return new ProfileResponse(user.getUserId(), user.getProfileName(), user.getProfileEmail());
+    }
+}
