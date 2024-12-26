81ef8755f55140e30432efdab828677be90f95ea
Han Kyobeom <ryqja18@gmail.com>
2024-12-26 10:09:35 +0900

    :adhesive_bandage : password requirement 수정

--- src/main/java/spartaspringnewspeed/spartafacespeed/common/exception/GlobalExceptionHandler.java
+++ src/main/java/spartaspringnewspeed/spartafacespeed/common/exception/GlobalExceptionHandler.java
@@ -2,8 +2,12 @@ package spartaspringnewspeed.spartafacespeed.common.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -32,5 +36,12 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(exception.getMessage(),HttpStatus.UNAUTHORIZED);
     }
 
-
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                errors.put(error.getField(), error.getDefaultMessage())
+        );
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
 }
